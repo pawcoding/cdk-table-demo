da4beb85c7b728a9d65c9f4bd9ea751f3c5c88df
pawcode Development <development@pawcode.de>
2026-02-07 17:43:14 +0100

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CdkTableModule } from '@angular/cdk/table';
+
+@Component({
+  selector: 'app-root',
+  imports: [CdkTableModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {}
